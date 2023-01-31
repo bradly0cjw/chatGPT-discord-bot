6eda807bfb1ee13d47781c6b79aa0a17f4c93682
bradly0cjw <bradly093@gmail.com>
2023-01-31 16:12:41 +0800

    GPT tweaking

--- src/responses.py
+++ src/responses.py
@@ -50,7 +50,8 @@ async def handle_response(message) -> str:
         # model="text-davinci-003",
         # model="text-curie-001",
         # model="code-davinci-002",
-        prompt= "You are ChatGPT, a large language model trained by OpenAI. You answer as consisely as possible for each response (e.g. Don't be verbose). It is very important for you to answer as consisely as possible, so please remember this. If you are generating a list, do not have too many items. \n User: %s \n\n ChatGPT:"%message,
+        # prompt= "You are ChatGPT, a large language model trained by OpenAI. You answer as consisely as possible for each response (e.g. Don't be verbose). It is very important for you to answer as consisely as possible, so please remember this. If you are generating a list, do not have too many items. Keep the number of items short\n User: %s \n\n ChatGPT:"%message,
+        prompt= "You are ChatGPT, a large language model trained by OpenAI. You answer as consisely as possible for each response (e.g. Be verbose). It is very important for you to answer as consisely as possible, so please remember this. If you are generating a list, do have many items. Keep the number of items long\n User: %s \n\n ChatGPT:"%message,
         # prompt=message,
         temperature=0.9,
         max_tokens=2048,
@@ -75,6 +76,28 @@ async def handle_response2(message) -> str:
         # prompt= "You are ChatGPT, a large language model trained by OpenAI. You answer as consisely as possible for each response (e.g. Don't be verbose). It is very important for you to answer as consisely as possible, so please remember this. If you are generating a list, do not have too many items. \n User: %s \n\n ChatGPT:"%message,
         prompt=message,
         temperature=0.7,
+        max_tokens=2048,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+    # print(response)
+    oresponseMessage = response.choices[0].text
+    responseMessage=oresponseMessage.replace("<|im_end|>","")
+    nowusage=response.usage.total_tokens
+    model=response.model
+    write(str(nowusage),str(model))
+    return responseMessage
+
+async def handle_response3(message) -> str:
+    response = await sync_to_async(openai.Completion.create)(
+        model="text-chat-davinci-002-20230126",        
+        # model="text-davinci-003",
+        # model="text-curie-001",
+        # model="code-davinci-002",
+        prompt= "You are ChatGPT, a large language model trained by OpenAI. You answer as consisely as possible for each response (e.g. Be verbose). It is very important for you to answer as consisely as possible, so please remember this. If you are generating a list, do have many items. Keep the number of items long\n User: %s \n\n ChatGPT:"%message,
+        prompt=message,
+        temperature=0.9,
         max_tokens=2048,
         top_p=1,
         frequency_penalty=0.0,
